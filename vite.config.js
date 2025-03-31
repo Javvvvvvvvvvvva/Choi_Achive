1a58947e101c2f7db0c6b563c231e1f3c6ccf1f8
javaking <97857545+Javvvvvvvvvvvva@users.noreply.github.com>
2025-03-31 19:07:37 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import restart from 'vite-plugin-restart'
 
+// vite.config.js
 export default {
-  root: 'src', // Tell Vite your index.html lives here
-  publicDir: '../static',
-  build: {
-    outDir: '../dist', // Output in root-level 'dist'
-    emptyOutDir: true,
-    sourcemap: true
-  },
-  plugins: [
-    restart({ restart: ['../static/**'] })
-  ]
-}
+    publicDir: 'static',
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: true
+    },
+    plugins: [
+      restart({ restart: ['static/**'] })
+    ]
+  }
+  
