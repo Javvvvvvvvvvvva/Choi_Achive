7d59f103b21fb7fe4de7bd4dd1f339a18b11e0b8
javaking <97857545+Javvvvvvvvvvvva@users.noreply.github.com>
2025-04-08 21:43:35 +0000

    cdn Change

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { OBJLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/OBJLoader.js';
-import { TextureLoader } from "three";
 
 const canvas = document.querySelector(".webgl");
 const label1 = document.getElementById("label1");
